931c583b3a3873ac7f5e24ff9be4920f33468bf8
Nicholas Huynh <nphuynh11@gmail.com>
2021-04-13 18:58:11 -0400

    updating models

--- backend/workout_tracker/models.py
+++ backend/workout_tracker/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Program(models.Model):
-    p_id = models.AutoField(primary_key=True)
-    p_type = models.CharField(max_length=200)
+    # p_id = models.AutoField(primary_key=True)
+    program_type = models.CharField(max_length=200)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(default=None)
     resets = models.IntegerField(default=0)
 
 class Workout(models.Model):
-    wo_id = models.AutoField(primary_key=True)
-    p_id = models.ForeignKey(Program, on_delete=models.CASCADE)
+    # wo_id = models.AutoField(primary_key=True)
+    pprogram = models.ForeignKey(Program, on_delete=models.CASCADE)
     date = models.DateField()
 
 class Excersie(models.Model):
-    ex_id = models.AutoField(primary_key=True)
-    wo_id = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    # ex_id = models.AutoField(primary_key=True)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     sets = models.IntegerField(default=0)
     reps = models.IntegerField(default=0)
