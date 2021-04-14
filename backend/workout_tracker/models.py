406c8f5622a6d6a540f9d20c30b42edfed603959
Nicholas Huynh <nphuynh11@gmail.com>
2021-04-14 19:52:01 -0400

    model changes

--- backend/workout_tracker/models.py
+++ backend/workout_tracker/models.py
@@ -1,22 +1,36 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Program(models.Model):
-    # p_id = models.AutoField(primary_key=True)
     program_type = models.CharField(max_length=200)
     start_date = models.DateField()
-    end_date = models.DateField(default=None)
+    end_date = models.DateField(blank=True, null=True)
     resets = models.IntegerField(default=0)
 
-class Workout(models.Model):
-    # wo_id = models.AutoField(primary_key=True)
-    pprogram = models.ForeignKey(Program, on_delete=models.CASCADE)
-    date = models.DateField()
+    def __str__(self):
+        return self.program_type + " Program " + str(self.id)
 
-class Excersie(models.Model):
-    # ex_id = models.AutoField(primary_key=True)
+    def program_length(self):
+        if self.end_date:
+            return self.end_date - self.start_date
+        else:
+            return timezone.now() - self.start_date
+
+class Workout(models.Model):
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    date = models.DateField()
+    theme = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.theme + " Workout " + str(self.id)
+
+class Exercise(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     sets = models.IntegerField(default=0)
     reps = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name + " " + str(self.id)
